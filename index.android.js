8dc316ef6fac41757420676c5802f92e605b2591
Swimming2014 <22273203+Swimming2014@users.noreply.github.com>
2017-08-18 16:16:33 +0800

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+var Main = require('./component/main/QLMain')
+export default class MyApp extends Component {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+AppRegistry.registerComponent('MyApp', () => MyApp);
